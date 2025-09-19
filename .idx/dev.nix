35b56fc8e8e723d2adf8dcd85169850f6f7368be
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-09-19 19:46:09 +0200

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -18,7 +18,7 @@
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        npm-install = "pnpm -s install --frozen-lockfile";
+        npm-install = "pnpm install --frozen-lockfile";
         # Open editors for the following files by default, if they exist:
         default.openFiles = [ "src/app/app.component.ts" ];
       };
